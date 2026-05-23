190d94463e165d58834f33e41cbab3df50f96d5b
Imad Mazzi <imadofficiel14@gmail.com>
2026-05-23 20:16:54 +0000

    Fix: Increase express json body limit to 50mb to allow debug db uploads

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ const { initAutomation } = require('./automation');
 // Middleware
 app.use(compression());
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Servir les fichiers statiques
 app.use(express.static(path.join(__dirname, '../public')));
