7093633b437be3ce1084d7b8162d6e224a523769
Imad Mazzi <imadofficiel14@gmail.com>
2026-05-23 19:12:59 +0000

    Fix: Add timestamp to Axios GET requests to bypass frontend caching

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -9,6 +9,18 @@ api.interceptors.request.use(config => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+  
+  // Force bypassing of any browser or edge cache
+  if (config.method === 'get') {
+    config.params = {
+      ...config.params,
+      _t: new Date().getTime()
+    };
+    config.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+    config.headers['Pragma'] = 'no-cache';
+    config.headers['Expires'] = '0';
+  }
+  
   return config;
 });
 
