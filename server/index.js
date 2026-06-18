0ac4e757a50e3a7de034cb6e44d61bbccf84ce06
Imad Mazzi <imadofficiel14@gmail.com>
2026-06-18 19:36:46 +0000

    fix: remove legacy sitemap.xml and robots.txt hardcoded endpoints

--- server/index.js
+++ server/index.js
@@ -358,37 +358,6 @@ app.use('/api/articles', require('./routes/articles'));
 app.use('/api/stats', require('./routes/stats'));
 app.use('/api/chat', require('./routes/chat'));
 
-// Sitemap dynamique
-app.get('/sitemap.xml', (req, res) => {
-  const db = require('./db');
-  const concours = db.prepare('SELECT slug, created_at FROM concours').all();
-  const articles = db.prepare('SELECT slug, created_at FROM articles').all();
-  const baseUrl = 'https://atlasconcours.ma';
-
-  let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>${baseUrl}/</loc><changefreq>daily</changefreq><priority>1.0</priority></url>
-  <url><loc>${baseUrl}/concours.html</loc><changefreq>daily</changefreq><priority>0.9</priority></url>
-  <url><loc>${baseUrl}/emplois.html</loc><changefreq>daily</changefreq><priority>0.8</priority></url>
-  <url><loc>${baseUrl}/blog.html</loc><changefreq>weekly</changefreq><priority>0.7</priority></url>`;
-
-  concours.forEach(c => {
-    xml += `\n  <url><loc>${baseUrl}/concours-detail.html?slug=${c.slug}</loc><changefreq>weekly</changefreq><priority>0.8</priority><lastmod>${c.created_at.slice(0,10)}</lastmod></url>`;
-  });
-  articles.forEach(a => {
-    xml += `\n  <url><loc>${baseUrl}/article-detail.html?slug=${a.slug}</loc><changefreq>monthly</changefreq><priority>0.6</priority><lastmod>${a.created_at.slice(0,10)}</lastmod></url>`;
-  });
-
-  xml += `\n</urlset>`;
-  res.header('Content-Type', 'application/xml');
-  res.send(xml);
-});
-
-// Robots.txt
-app.get('/robots.txt', (req, res) => {
-  res.type('text/plain');
-  res.send(`User-agent: *\nAllow: /\nDisallow: /admin/\nSitemap: https://atlasconcours.ma/sitemap.xml`);
-});
 
 // Redirect legacy .html pages to React routes
 app.get('/index.html', (req, res) => res.redirect(301, '/'));
